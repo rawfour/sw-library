f8b51b8e007fa7093d9c76e78383c73bdb9b1101
Rafal <rdziedziela1@gmail.com>
2021-04-07 01:26:11 +0200

    add overweight couter logic

--- src/components/CharCard.vue
+++ src/components/CharCard.vue
@@ -1,13 +1,40 @@
 <template>
-    <div :style="{'color': charData.eye_color}" class="char-card">{{ charData.name }}</div>
+    <div :style="{'color': charData.eye_color}" class="char-card">{{ charData.name }} <span>overweight: {{ isOverweight }}</span>  <span>overweight value: {{ overweight }}</span></div>
 </template>
 
 
 <script>
 export default {
     name: "CharacterCard",
+    data(){
+        return{
+            isOverweight: false,
+            overweight: null,
+        }
+    },
     props: {
         charData: Object
+    },
+    computed: {
+
+    },
+    mounted(){
+            const mass = this.charData.mass.replace(',', '').trim(),
+                  height = this.charData.height.replace(',', '').trim(),
+                  bmi = Math.round(mass/(height/100*height/100));
+
+            if(!isNaN(bmi)){
+                if(bmi > 25){
+                    this.isOverweight = true;
+                    const correctWeight = 25 * (height/100*height/100);
+
+                    this.overweight = Math.round(mass - correctWeight);
+
+                }else{
+                    this.isOverweight = false;
+                }
+            }
     }
+    
 }
 </script>
