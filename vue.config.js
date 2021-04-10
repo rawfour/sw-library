4a8a9ae1aa8968d067cce1d08c8f68fd700d4228
Rafal <rdziedziela1@gmail.com>
2021-04-10 22:07:49 +0200

    add deployment script

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/sw-library/'
-  : '/',
+    publicPath: '/sw-library/',
     css: {
       loaderOptions: {
         sass: {
